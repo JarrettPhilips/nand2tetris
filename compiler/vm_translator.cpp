e603896d4a768f13ee4cee384a24a25b0cafe3e5
jarrettphilips <jarrettphilips@gmail.com>
2025-07-30 22:23:34 -0700

    build system for compiler

---
+++ compiler/vm_translator.cpp
@@ -0,0 +1,5 @@
+#ifndef TEST_MODE
+int main(int argc, char *argv[]){
+
+}
+#endif
---
+++ compiler/vm_translator_test.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+#include "vm_translator.h"
+
+TEST_CASE("Check is_number", "[helper]") {
+    // REQUIRE(is_number("3") == true);
+    // REQUIRE(is_number("n") == false);
+}
